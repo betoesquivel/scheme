0006d24de3426dc8a43d4b69564d4051ae21c8d4
Beto Esquivel <ppbeto94@gmail.com>
2014-09-23 01:14:11 -0500

    Finished activity

---
+++ actividadClaseRecursividadProfunda.rkt
@@ -0,0 +1,38 @@
+; Calcula la profundidad de una lista
+(
+ define (profundidad lista)
+  (
+   cond ((null? lista) 
+         0); caso base
+        ((atom? (car lista)) 
+         (profundidad (cdr lista)))
+        (else 
+         (max (+ 1 (profundidad (car lista)))
+                   (profundidad (cdr lista))))
+  )
+)
+
+; Checks if a is an atom or not
+(
+ define (atom? a)
+  (not (pair? a))
+)
+
+; Constructs simetrical pattern with N nesting levels. 
+(define (simetrico n) (simetrico-aux n '()))
+
+; Helper function for the simetrico function.
+(define (simetrico-aux n lista)
+        (if (zero? n)
+                lista
+        (simetrico-aux (- n 1) (list '< lista '> ))
+))     
+ 
+(define (elimina x lista)
+        (cond
+                ((null? lista) lista)
+                ((list? (car lista)) (cons (elimina x (car lista)) (elimina x (cdr lista))))
+                ((equal? (car lista) x) (elimina x (cdr lista)))
+                (else (cons (car lista) (elimina x (cdr lista))))
+        )
+)
